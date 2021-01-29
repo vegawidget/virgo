053103b8541bdbc7fcf58550ab3c3d4040b66f50
sa-lee <lee.s@wehi.edu.au>
2021-01-29 11:23:05 +1100

    reexport methods from vegawdiget

---
+++ R/utils-vegawidget.R
@@ -0,0 +1,44 @@
+# nocov start
+
+#### vegaspec functions ####
+#### print functions ####
+
+#' Knit-print method
+#'
+#' See \code{vegawidget::\link[vegawidget]{knit_print.vegaspec}} for details,
+#' particularly on additional packages that may have to be installed.
+#'
+#' @name knit_print.vegaspec
+#' @rdname knit_print.vegaspec
+#' @importFrom vegawidget knit_print.vegaspec
+#' @export
+#'
+NULL
+
+#' Set base URL
+#'
+#' See \code{vegawidget::\link[vegawidget]{vw_set_base_url}} for details.
+#'
+#' @name vw_set_base_url
+#' @rdname vw_set_base_url
+#' @importFrom vegawidget vw_set_base_url
+#' @export
+#'
+NULL
+
+#### image functions ####
+
+#' Create or write image
+#'
+#' See \code{vegawidget::\link[vegawidget]{image}} for details.
+#'
+#' @name image
+#' @importFrom vegawidget
+#'           vw_to_svg vw_to_bitmap vw_write_png vw_write_svg
+#' @aliases  vw_to_svg vw_to_bitmap vw_write_png vw_write_svg
+#' @export   vw_to_svg vw_to_bitmap vw_write_png vw_write_svg
+#'
+NULL
+
+
+# nocov end
