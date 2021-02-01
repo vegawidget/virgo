0480ed374c46fe7914836f0856a5d00edee65348
sa-lee <lee.s@wehi.edu.au>
2021-02-01 11:37:33 +1100

    update melbweather to a summer and winter month #15

--- R/datasets.R
+++ R/datasets.R
@@ -2,18 +2,18 @@
 #'
 #' @details This data comes from the [Melbourne Open Data Portal](https://data.melbourne.vic.gov.au/Environment/Microclimate-Sensor-Readings/u4vh-84j8)
 #' and contains measurements from microclimate sensors around the city. Here
-#' we have restricted the data to contain measurements from December 2019 until
-#' the end of Februrary 2020. There are five sites where measurements are taken
-#' every 15 minutes. The data is presented in wide form
+#' we have restricted the data to contain measurements from January 2020 and June 2020. There are five sites where measurements are taken
+#' every 15 minutes.
 #'
-#' @format A tibble with 43,027 rows and 9 variables:
+#' @format A tibble with 32,253 rows and 12 variables:
 #' * `site`: Site identifier, this is the location of the weather sensor,
-#' there are five sites
+#' there are five sites located around the city.
+#' * `site_address`: The address of the site
 #' * `longitude, latitude`: The spatial coordinates of the measurement sites
 #' * `date_time`: The local date time that a sensor made a recording
 #' * `date`: Date associated with `date_time`
 #' * `ambient_temperature`: The value of the ambient air temperature in degrees Celsius.
-#' * `relative_humidity`: The percent value of the relative humidity
+#' * `relative_humidity`: The percent value of the relative humidity (no units)
 #' * `barometric_pressure`: The barometric pressure in hectopascals (hPa)
 #' * `wind_speed`: The wind speed in kilometers per hour (km/h)
 #' * `pm2.5,pm10`: The mass density of particulate matter in the air less than 2.5 (10) micrometers in diameter. Measured in micrograms per cubic meter.
--- data-raw/melbweather.R
+++ data-raw/melbweather.R
@@ -1,43 +1,60 @@
 ## code to prepare `melbweather` dataset goes here
 # update once rwalkr gets put up on cran
 # remotes::install_github("earowang/rwalkr@d528e7b")
-
-# well we use three months of data for summer 2019/20
+# well we use January and June 2020
 library(rwalkr)
 library(dplyr)
-start <- as.Date("2019-12-01")
-end <- as.Date("2020-02-29")
 
-# extract regular measurements, not any averaged forms
-# variables are temperature, relative humidity, barometric pressure,
-# particular matter 2.5 and 10, and wind speed
-sensors <- c("TPH.TEMP", "TPH.RH", "TPH.PRESSURE", "PM2.5", "PM10", "WS")
+extract_measurements <- function(month_rng) {
+  # extract regular measurements, not any averaged forms
+  # variables are temperature, relative humidity, barometric pressure,
+  # particular matter 2.5 and 10, and wind speed
+  sensors <- c("TPH.TEMP",
+               "TPH.RH",
+               "TPH.PRESSURE",
+               "PM2.5",
+               "PM10",
+               "WS")
 
-sensors_clean <- c("ambient_temperature",
-                   "relative_humidity",
-                   "barometric_pressure",
-                   "pm2.5",
-                   "pm10",
-                   "wind_speed")
-names(sensors_clean) <- sensors
+  sensors_clean <- c("ambient_temperature",
+                     "relative_humidity",
+                     "barometric_pressure",
+                     "pm2.5",
+                     "pm10",
+                     "wind_speed")
 
+  names(sensors_clean) <- sensors
 
-melbweather <- melb_weather(start, end)
+  start <- month_rng[1]
+  end <- month_rng[2]
+
+  # download data from start and end,
+  # filter to relevant sensors and pivot to wide form
+  melb_weather(start, end) %>%
+    filter(sensor_type %in% sensors) %>%
+    mutate(
+      sensor_type = sensors_clean[sensor_type],
+      value = as.numeric(value)
+    ) %>%
+    tidyr::pivot_wider(
+      id_cols = c("site", "date_time", "date"),
+      names_from = sensor_type
+    )
+}
+
+months <- list(
+  jan = c(as.Date("2020-01-01"), as.Date("2020-01-31")),
+  jun = c(as.Date("2020-06-01"), as.Date("2020-06-30"))
+)
+
+melbweather <- bind_rows(lapply(months, extract_measurements))
+
+# pull in the sites coordinates and description
+sites <- select(pull_weather_sensors(),
+                site_id, description, longitude, latitude)
 
 melbweather <- melbweather %>%
-  filter(sensor_type %in% sensors) %>%
-  mutate(sensor_type = sensors_clean[sensor_type],
-         value = as.numeric(value))
-
-melbweather <- tidyr::pivot_wider(melbweather,
-                                  id_cols = c("site", "date_time", "date"),
-                                  names_from = sensor_type)
-# pull in the sites lat lon
-sites <- select(pull_weather_sensors(), site_id, longitude, latitude)
-
-melbweather <- left_join(melbweather, sites, by = c("site" = "site_id"))
-
-melbweather <- melbweather %>%
-  select(site, longitude, latitude, date_time, date, everything())
+  left_join(sites, by = c("site" = "site_id")) %>%
+  select(site,  site_address = description, longitude, latitude, date_time, date, everything())
 
 usethis::use_data(melbweather, overwrite = TRUE)
